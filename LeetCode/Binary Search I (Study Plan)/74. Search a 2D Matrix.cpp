2b68da403b5300f092ce3ab5163f9d1b2d32c847
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-05-08 12:07:33 +0600

    Create 74. Search a 2D Matrix.cpp

---
+++ LeetCode/Binary Search I (Study Plan)/74. Search a 2D Matrix.cpp
@@ -0,0 +1,16 @@
+// link: https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i = matrix.size(), j = matrix[0].size();
+        int start = 0, end = i*j-1;
+        while(start<=end) {
+            int mid = start+(end-start)/2;
+            if (matrix[mid/j][mid%j] == target) return true;
+            if (matrix[mid/j][mid%j] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return false;
+    }
+};
