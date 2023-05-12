e2ae55d8f43fd10bd3ad4f5c64e539e6f6c73813
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-05-12 15:32:06 +0600

    Create sol.java

---
+++ chess_in_programming/1st-edition/chessboard squares/sol.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IsWhite {
+
+    public static boolean isWhite(String s) {
+        int x1 = s.charAt(0) - 'A' + 1;
+        int x2 = s.charAt(1) - '0';
+        return (x1 + x2) % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        if (isWhite(s)) {
+            System.out.println("WHITE");
+        } else {
+            System.out.println("BLACK");
+        }
+    }
+}
