4089977a5ebe13c67c3b7a249611802b02964231
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-05-12 16:02:50 +0600

    Create sol.cpp

---
+++ chess_in_programming/1st-edition/chess knight/sol.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void letter(int x){
+  char s=(char)('a'+x-1);
+  cout<<s;
+}
+
+int main() {
+  string s;
+  cin >> s;
+  int x = s[0] - 'a' + 1;
+  int y = s[1] - '0';
+  if(y-1>0 && y-1<9 && x-2>0 && x-2<9){
+    letter(x-2);
+    cout<<y-1<<endl;
+  }
+  if(y+1>0 && y+1<9 && x-2>0 && x-2<9){
+    letter(x-2);
+    cout<<y+1<<endl;
+  }
+  if(y-2>0 && y-2<9 && x-1>0 && x-1<9){
+    letter(x-1);
+    cout<<y-2<<endl;
+  }
+  if(y+2>0 && y+2<9 && x-1>0 && x-1<9){
+    letter(x-1);
+    cout<<y+2<<endl;
+  }
+  if(y-2>0 && y-2<9 && x+1>0 && x+1<9){
+    letter(x+1);
+    cout<<y-2<<endl;
+  }
+  if(y+2>0 && y+2<9 && x+1>0 && x+1<9){
+    letter(x+1);
+    cout<<y+2<<endl;
+  }
+  if(y-1>0 && y-1<9 && x+2>0 && x+2<9){
+    letter(x+2);
+    cout<<y-1<<endl;
+  }
+  if(y+1>0 && y+1<9 && x+2>0 && x+2<9){
+    letter(x+2);
+    cout<<y+1<<endl;
+  }
+  return 0;
+}
