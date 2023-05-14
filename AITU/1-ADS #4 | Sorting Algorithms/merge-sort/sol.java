6e4eb0206c15bfe008adfb3f2449b56bd87075d3
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-05-14 20:53:49 +0600

    Update sol.java

--- AITU/1-ADS #4 | Sorting Algorithms/merge-sort/sol.java
+++ AITU/1-ADS #4 | Sorting Algorithms/merge-sort/sol.java
@@ -29,46 +29,53 @@ public class MergeSort {
         }
     }
 
-    public static void mergeSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int mid = (low + high) / 2;
-            mergeSort(arr, low, mid);
-            mergeSort(arr, mid + 1, high);
-            merge(arr, low, mid, high);
-        }
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
+    
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
 
-    public static void merge(int[] arr, int low, int mid, int high) {
-        int[] temp = new int[high - low + 1];
-        int i = low;
-        int j = mid + 1;
-        int k = 0;
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
 
-        while (i <= mid && j <= high) {
-            if (arr[i] <= arr[j]) {
-                temp[k] = arr[i];
+        for (int i = 0; i < n1; i++)
+            leftArr[i] = arr[left + i];
+
+        for (int i = 0; i < n2; i++)
+            rightArr[i] = arr[mid + 1 + i];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
                 i++;
-            } else {
-                temp[k] = arr[j];
+            }
+            else {
+                arr[k] = rightArr[j];
                 j++;
             }
             k++;
         }
 
-        while (i <= mid) {
-            temp[k] = arr[i];
+        while (i < n1) {
+            arr[k] = leftArr[i];
             i++;
             k++;
         }
 
-        while (j <= high) {
-            temp[k] = arr[j];
+        while (j < n2) {
+            arr[k] = rightArr[j];
             j++;
             k++;
-        }
-
-        for (i = 0; i < k; i++) {
-            arr[low + i] = temp[i];
         }
     }
 }
