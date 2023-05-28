1f3ef42c2484e4f4e7ef4b41b1a9af8d304553b1
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-05-28 23:32:22 +0600

    Create aitu-open: first day.cpp

---
+++ Competitions/aitu-open: first day.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define read(type) readInt<type>() // Fast read
+#define ll long long
+#define nL "\n"
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+
+string solve(ll a, ll b, ll c) {
+	if(a+b>13) return "NO";
+	if(a+c>13) return "NO";
+	return "YES";
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll a, b, c;
+	cin>>a>>b>>c;
+    cout<<solve(a,b,c);
+    return 0;
+}
