47fbd42ade629b216b0007b32dca61d7807765e0
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-07-27 16:41:18 +0600

    Create G - Planning The Expedition.py

---
+++ Competitions/cpfed-bootcamp-day-1/G - Planning The Expedition.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+def solve(n, food):
+    freq = Counter(food)
+    poss = sorted(freq.values())
+
+    theory_max = sum(poss) // n
+
+    for i in range(theory_max, 0, -1):
+        cnt = 0
+        for p in poss:
+            while p-i >= 0:
+                p -= i
+                cnt += 1
+        if cnt >= n:
+            return i
+
+    return 0
+
+if __name__ == '__main__':
+    n, m = list(map(int, input().strip().split()))
+    food = list(map(int, input().strip().split()))
+    print(solve(n, food))
