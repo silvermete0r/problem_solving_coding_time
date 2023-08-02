eca65fd6c228d4bc49a5103506d242f644ed4e4c
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-08-02 10:59:28 +0600

    Create cpp_coding_template [August 2023].cpp

---
+++ cpp_coding_template [August 2023].cpp
@@ -0,0 +1,29 @@
+// link: task permalink
+
+#include <bits/stdc++.h>
+
+#define uset unordered_set
+#define umap unordered_map
+#define ivec vector<int>
+#define pii pair<int, int>
+#define pb push_back
+#define mk make_pair
+#define all(x) (x).begin(), (x).end()
+
+#define MOD 1e7
+#define ll long long
+#define iMax INT_MAX
+#define iMin INT_MIN
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+ 
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	//	TxtIO;
+	solve();
+    return 0;
+}
