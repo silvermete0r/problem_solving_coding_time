44945913ccf86e405030d258821ba3ca435bd9bc
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-08-03 16:09:04 +0600

    Create task-K [Возведение в степень].cpp

---
+++ Competitions/cpfed-bootcamp-day-8/task-K [Возведение в степень].cpp
@@ -0,0 +1,56 @@
+// link: task permalink
+
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+#define uset unordered_set
+#define umap unordered_map
+#define ivec vector<int>
+#define pii pair<int, int>
+#define pb push_back
+#define mk make_pair
+#define all(x) (x).begin(), (x).end()
+
+#define ll long long
+#define iMax INT_MAX
+#define iMin INT_MIN
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+ 
+using namespace std;
+
+const ll MOD = 1000000007;
+
+bool isPrime(ll n) {
+	if(n<=1) return false;
+	for(ll i=2; i<=sqrt(n); i++) {
+		if(n%i==0) return false;
+	}
+	return true;
+}
+
+ll gcd(ll a, ll b) {
+	return b ? gcd (b, a % b) : a;
+}
+
+ll binpow(ll a, ll n) {
+	int res = 1;
+	while(n) {
+		if(n&1) res = (res * a)%MOD;
+		a = (a * a)%MOD;
+		n >>= 1;
+	}
+	return res;
+}
+
+void solve() {}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	//	TxtIO;
+	ll a, b;
+	cin>>a>>b;
+	cout<<binpow(a, b)%MOD;
+    return 0;
+}
