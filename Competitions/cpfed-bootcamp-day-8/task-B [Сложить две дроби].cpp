bec0bd6eb063beeafe577104bf5295c47b10274d
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-08-03 16:02:36 +0600

    Create task-B [Сложить две дроби].cpp

---
+++ Competitions/cpfed-bootcamp-day-8/task-B [Сложить две дроби].cpp
@@ -0,0 +1,54 @@
+// link: task permalink
+
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+#define uset unordered_set
+#define umap unordered_map
+#define ivec vector<int>
+#define pii pair<int, int>
+#define pb push_back
+#define mk make_pair
+#define all(x) (x).begin(), (x).end()
+
+#define MOD 1e7
+#define ll long long
+#define iMax INT_MAX
+#define iMin INT_MIN
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+ 
+using namespace std;
+
+bool isPrime(ll n) {
+	if(n<=1) return false;
+	for(int i=2; i<=sqrt(n); i++) {
+		if(n%i==0) return false;
+	}
+	return true;
+}
+
+ll binpow(ll a, ll n) {
+	int res = 1;
+	while(n) {
+		if(n&1) res*=a;
+		a *= a;
+		n >>= 1;
+	}
+	return res;
+}
+
+void solve() {}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	//	TxtIO;
+	ll a, b, c, d;
+	cin>>a>>b>>c>>d;
+	int m = (b*d)/__gcd(b, d);
+	int n = a*(m/b) + c*(m/d);
+	int del = __gcd(n, m);
+	cout<<n/del<<" "<<m/del;
+    return 0;
+}
