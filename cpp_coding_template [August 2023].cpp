08296d311362a00adee9ef0d19ea4687b134cdbb
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-08-04 17:01:38 +0600

    Update cpp_coding_template [August 2023].cpp

--- cpp_coding_template [August 2023].cpp
+++ cpp_coding_template [August 2023].cpp
@@ -6,18 +6,25 @@
 
 #define uset unordered_set
 #define umap unordered_map
-#define ivec vector<int>
+#define vi vector<int>
+#define vii vector<int, int>
 #define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
 #define pb push_back
+#define eb emplace_back
 #define mk make_pair
 #define all(x) (x).begin(), (x).end()
 
 #define MOD 1e7
+#define PI 3.1415926535897932384626433832795
 #define ll long long
 #define iMax INT_MAX
 #define iMin INT_MIN
 #define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
- 
+
 using namespace std;
 
 void solve() {}
@@ -25,7 +32,8 @@ void solve() {}
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	//	TxtIO;
-	solve();
+    cout.tie(NULL);
+    //	TxtIO;
+	
     return 0;
 }
