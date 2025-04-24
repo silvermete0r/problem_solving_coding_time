fba0bdfd5efcaa7f319638f7f7b40c2fb470b465
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2025-04-24 19:32:28 +0500

    Create cpp_coding_template [April 2025].cpp

---
+++ cpp_coding_template [April 2025].cpp
@@ -0,0 +1,27 @@
+// link: task permalink
+
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+
+const PI = 3.1415926535897932384626433832795;
+const MOD = 1e9 + 7;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //	TxtIO;
+    int t = 1;    
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
