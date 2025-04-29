649a2b758410f80236aa35803aae20b49239f899
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2025-04-29 10:12:10 +0500

    Update cpp_coding_template [April 2025].cpp

--- cpp_coding_template [April 2025].cpp
+++ cpp_coding_template [April 2025].cpp
@@ -7,12 +7,13 @@
 #include <bits/stdc++.h>
 
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define ll long long
 #define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
 
-const double PI = 3.1415926535897932384626433832795;
-const int INF = 1e9 + 7;
+const int PI = 3.1415926535897932384626433832795;
 const int MOD = 1e9 + 7;
+const int INF = 1e9 + 7;
 
 using namespace std;
 
@@ -31,6 +32,6 @@ int32_t main() {
     //	TxtIO;
 	int t = 1;
 	cin>>t;
-	while(t--) solve();
+	while(t--) solve(vec);
     return 0;
 }
